c4e3355e10def688fc252eb63af78029d79555e4
SHIVAM PATHAK <sp254917@gmail.com>
2025-11-18 09:36:15 +0530

    Update index.js agin

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ app.set("io",io)
 
 const port=process.env.PORT || 5000
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://quickbite12.netlify.app",
     credentials:true
 }))
 app.use(express.json())
